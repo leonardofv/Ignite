24627f976ce0ef772738d46995a102694ee6b4f3
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-04-02 16:46:07 -0300

    styles sideBar

--- src/components/sideBar/sideBar.jsx
+++ src/components/sideBar/sideBar.jsx
@@ -1,4 +1,5 @@
 import S from './sideBar.module.css';
+import { PencilLine } from '@phosphor-icons/react';
 
 export function SideBar() {
     return (
@@ -7,6 +8,7 @@ export function SideBar() {
             <img className={S.cover} src="https://bairesdev.mo.cloudinary.net/blog/2023/09/How-Many-Web-Developers-in-the-World-1.jpg?tx=w_500,q_50" />
 
             <div className={S.profile}>
+                <img className={S.avatar} src="https://github.com/leonardofv.png" alt="foto de perfil" />
                 <strong>Leonardo</strong>
                 <span>
                     Developer
@@ -15,6 +17,7 @@ export function SideBar() {
 
             <footer className={S.footer}>
                 <a href="">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
