6f7763fccfe55d52bfdb8a7db3e6c4e9bcf38093
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-04-13 13:42:08 -0300

    change profile and avatar

--- src/components/sideBar/sideBar.jsx
+++ src/components/sideBar/sideBar.jsx
@@ -4,9 +4,18 @@ export function SideBar() {
     return (
         <aside className={S.sideBar}>
             
-            <img className={S.cover} src="https://bairesdev.mo.cloudinary.net/blog/2023/09/How-Many-Web-Developers-in-the-World-1.jpg?tx=w_500,q_50" />
+            <img 
+                className={S.cover} 
+                src="https://bairesdev.mo.cloudinary.net/blog/2023/09/How-Many-Web-Developers-in-the-World-1.jpg?tx=w_500,q_50" 
+            />
 
             <div className={S.profile}>
+
+                <img 
+                    className={S.avatar} 
+                    src="https://github.com/leonardofv.png" 
+                />
+
                 <strong>Leonardo</strong>
                 <span>
                     Developer
