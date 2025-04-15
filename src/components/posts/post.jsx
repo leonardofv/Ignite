0d2471e3082efc9b1b1b6f418b015c4ba67a338b
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-04-14 21:02:38 -0300

    start comment

--- src/components/posts/post.jsx
+++ src/components/posts/post.jsx
@@ -29,6 +29,18 @@ export function Post() {
                     <a href=""> #rocktseat</a>
                 </p>
             </div>
+
+            <form className={S.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea 
+                    placeholder='Deixe um comentário'
+                />
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
